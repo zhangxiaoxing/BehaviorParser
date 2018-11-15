94ba6a26e8b4903a5b40f535fdddbb918496bddf
ZX <wwweagle@gmail.com>
2018-11-15 20:16:46 +0800

    some house cleaning

--- Gain_Of_Function/sortFiles.m
+++
@@ -1,5 +0,0 @@
-function out=sortFiles(files)
-ids=regexp(files(:,1),'(?<=\\)\w?\d{1,4}(?=_)','match','once');
-[~,I]=sort(ids);
-out=files(I,:);
-end
--- individual.m
+++
@@ -1,6 +0,0 @@
-f=z.listFiles({'83','Base','-NDelay'})
-for i=1:size(f,1)
-z.processFile(char(f(i)));
-r=[z.getHitFalseMiss(false,50)',z.getHitFalseMiss(true,50)'];
-disp(r)
-end
